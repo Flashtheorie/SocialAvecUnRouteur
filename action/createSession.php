bd0719e864541fc1b813bb907f4735a79c38c3f8
Antoine <antoinepascualmail@gmail.com>
2022-03-19 14:02:50 +0100

    Ajout d'un prénom et un nom au hasard

--- action/createSession.php
+++ action/createSession.php
@@ -1,9 +1,10 @@
 <?php 
 session_start();
-
+require './class/functions.php'; 
 $rand = rand(0,9999);
 
-$_SESSION['id'] = $rand;
+GenerateNoms();
+
 
 header('Location: home');
 
--- views/navbar.php
+++ views/navbar.php
@@ -1,5 +1,8 @@
 <?php session_start();?>
 
+<?php require './class/functions.php'; ?>
+
+
 
 <script src="https://kit.fontawesome.com/2fc09f2ce5.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
